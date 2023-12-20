9b789743f16ad11e056e01f0e457428b5dd03bca
alex.herrera <alex.guialexander@gmail.com>
2023-12-19 22:05:57 -0500

    star

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    quotes: [1, 'single', { avoidEscape: true }],
+    jsx-quotes: [2, 'prefer-single']
+  }
+};
---
+++ README.md
@@ -0,0 +1,19 @@
+# titulo
+
+normalizado listo para crear el proyecto
+
+## Description
+
+
+
+## How to use
+
+clonar
+
+## Author
+
+Guillermo Alexander Herrera Rodriguez
+
+## Acknowledgments
+
+
