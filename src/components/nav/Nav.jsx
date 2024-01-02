8dfa2545595f70e0b3e3822a2d751c5263f34070
alex.herrera <alex.guialexander@gmail.com>
2024-01-02 15:33:21 -0500

    feat: nav media

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React , { useState }from 'react'
 import { NavLink } from 'react-router-dom';
 import './nav.css'
 
 const Nav = () => {
+
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const handleMenuHover = () => {
+    setMenuVisible(!menuVisible);
+  };
   return (
     <nav className="nav">
-    <ul>
-        <NavLink to='/'> HOME </NavLink>
-        <NavLink to='/about'> Quiénes Somos</NavLink>
-        <NavLink to='/consultoriatransformaciondigital'> Consultoría Transformación Digital</NavLink>
-        <NavLink to='/sipa'>SIPA</NavLink>
-        <NavLink to='/businessintellence'>Business Intellence</NavLink>
-    </ul>
+    <div className="menu">
+      <ul className={menuVisible ? 'menu-list show' : 'menu-list'}>
+        <li><NavLink to='/'> HOME </NavLink></li>
+        <li><NavLink to='/about'> Quiénes Somos</NavLink></li>
+        <li><NavLink to='/consultoriatransformaciondigital'> Consultoría Transformación Digital</NavLink></li>
+        <li><NavLink to='/sipa'>SIPA</NavLink></li>
+        <li><NavLink to='/businessintellence'>Business Intellence</NavLink></li>
+      </ul>
+    </div>
+    <div className={`menu-trigger ${menuVisible ? 'hide-trigger' : ''}`} onMouseEnter={handleMenuHover} onMouseLeave={handleMenuHover}>
+        <a href="#">Menú</a>
+      </div>
   </nav>
   )
 }
